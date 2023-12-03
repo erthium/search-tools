20718562c03dec8909121a2f82a2ff4a2f4bd510
Erthium <ertyum35@gmail.com>
2023-12-03 15:29:30 +0300

    Complete py solutions for recursive and iterative

--- src/main.py
+++ src/main.py
@@ -1,14 +1,3 @@
-"""
-int recursive_lev(const string& a, const string& b){
-    if (a.length() == 0) return b.length();
-    if (b.length() == 0) return a.length();
-    if (a[0] == b[0]) return recursive_lev(a.substr(1), b.substr(1));
-    return 1 + min(recursive_lev(a.substr(1), b),
-                    min(recursive_lev(a, b.substr(1)),
-                    recursive_lev(a.substr(1), b.substr(1))));
-}
-"""
-
 def recursive_lev(a:str, b:str) -> int:
     if len(a) == 0: return len(b)
     if len(b) == 0: return len(a)
@@ -20,39 +9,52 @@ def recursive_lev(a:str, b:str) -> int:
     ])
 
 
-"""
-int iterative_lev(const string& a, const string& b){
-    int a_len = a.length();
-    int b_len = b.length();
+def iterative_lev(a:str, b:str, print_matrix:bool = False) -> int:
+    a_len = len(a)
+    b_len = len(b)
 
-    int d[a_len + 1][b_len + 1] = {0};
-    for (int i = 1; i < a_len; i++){
-        d[i][0] = i;
-    }
-    for (int j = 1; j < b_len; j++){
-        d[0][j] = j;
-    }
+    if a_len == 0: return len(b)
+    if b_len == 0: return len(a)
 
-    for (int i = 1; i < a_len; i++)
-    {
-        for (int j = 1; j < b_len; j++)
-        {
-            int sub_cost = 1;
-            if (a[i - 1] == b[j - 1]) sub_cost = 0;
-            d[i][j] = min(d[i - 1][j] + 1,
-                          min(d[i][j - 1] + 1,
-                          d[i - 1][j - 1] + sub_cost));
-        }
-    }
-    return d[a_len - 1][b_len - 1];
-}
-"""
+    d = [[0 for _ in range(b_len + 1)] for _ in range(a_len + 1)]
 
-def iterative_lev(a:str, b:str) -> int:
-    pass    
+    for i in range(1, a_len + 1):
+        d[i][0] = i
+
+    for j in range(1, b_len + 1):
+        d[0][j] = j
+    
+    for i in range(1, a_len + 1):
+        for j in range(1, b_len + 1):
+            sub_cost = 1
+            if a[i - 1] == b[j - 1]: sub_cost = 0
+            d[i][j] = min([
+                d[i - 1][j] + 1,
+                d[i][j - 1] + 1,
+                d[i - 1][j - 1] + sub_cost
+            ])
+    
+    if print_matrix:
+        for i in range(a_len + 1):
+            for j in range(b_len + 1):
+                print(d[i][j], end=' ')
+            print()
+        print(f'Distance: {d[a_len][b_len]}')
+    return d[a_len][b_len]
+
+
 
 def main():
-    pass
+    iterative_lev("1234", "4321", True)
+    iterative_lev("a", "1234", True)
+    iterative_lev("asd", "asdd", True)
+
+    print(recursive_lev("1234", "4321"))
+    print(recursive_lev("a", "1234"))
+    print(recursive_lev("asd", "asdd"))
+    print(iterative_lev("asd", "asd"))
+
+
 
 if __name__ == '__main__':
     main()
