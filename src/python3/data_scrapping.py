50a2207ef59a5c88ade1fa069eeffff8df597672
Erthium <ertyum35@gmail.com>
2023-12-04 09:07:22 +0300

    Create a web-scrapping script for testing data
    Used wired.com for taking their news under the security section

---
+++ src/python3/data_scrapping.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+security_news_template = 'https://www.wired.com/category/security/?page={}'
+website_url_template = 'https://www.wired.com{}'
+link_class = 'SummaryItemHedLink-civMjp ejgyuy summary-item-tracking__hed-link summary-item__hed-link'
+"""
+Structure in website:
+<sections> : data-testid=SummaryRiverSection & class=SummaryRiverSection-kPnzzO fhMbrM
+    <a> : class=S
+    href={link}
+        <h3> -> Title
+
+
+"""
+
+with open('security_news.csv', 'w', encoding='utf-8') as file:
+    file.write('Title;;Link\n')
+    for i in range(1, 100):
+        print(f'Scrapping page: {i}')
+        url = security_news_template.format(i)
+        response = requests.get(url)
+        page_soup = BeautifulSoup(response.text, 'html.parser')
+        all_sections = page_soup.find_all('section', {'data-testid': 'SummaryRiverSection'})
+        for section in all_sections:
+            links = section.find_all('a', {'class': link_class})
+            for link in links:
+                title = link.find('h3').text
+                href = link['href']
+                news_response = requests.get(website_url_template.format(href))
+                news_soup = BeautifulSoup(news_response.text, 'html.parser')
+                time = news_soup.find('time').text
+                file.write(f'{title};;{href};;{time}\n')
+
+                
+    
